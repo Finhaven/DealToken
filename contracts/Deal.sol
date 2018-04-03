dd2d72ebad9e4498fd46940a648f61521665eb0d
Brooklyn Zelenka <be.zelenka@gmail.com>
2018-04-03 00:31:03 -0700

    Compiles

--- contracts/Deal.sol
+++ contracts/Deal.sol
@@ -1,20 +1,51 @@
 pragma solidity ^0.4.15;
 
 import '../node_modules/validated-token/contracts/ReferenceToken.sol';
-import '../node_modules/zepplin-solidity/contracts/token/ERC20/TokenTimelock.sol';
+import '../node_modules/zeppelin-solidity/contracts/token/ERC20/TokenTimelock.sol';
 
-contract Deal is ReferenceToken, TokenTimelock {
-  function Deal(
-      string         _name,
-      string         _symbol,
-      uint256        _granularity,
-      TokenValidator _validator
-  ) public {
-    ReferenceToken(_name, _symbol, _granularity, _validator);
-  }
+contract Deal is ReferenceToken {
+    using SafeMath for uint256;
 
-  function mint(address _tokenHolder, uint256 _amount) public onlyOwner {
-    escrow = TokenTimelock(this, _tokenHolder, now + 16 weeks);
-    super.mint(escrow, _amount);
-  }
+    uint256 public startTime;
+    uint256 public endTime;
+    uint256 public holdPeriod;
+
+    // Does this need to be mapping(address => TokenTimelock[])?
+    mapping(address => TokenTimelock) public holds;
+
+    function Deal(
+        string _name,
+        string _symbol,
+        uint256 _granularity,
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _holdPeriod,
+        TokenValidator _validator
+    ) ReferenceToken(_name, _symbol, _granularity, _validator) public {
+      require(_startTime >= now);
+      require(_startTime < _endTime);
+
+      startTime  = _startTime;
+      endTime    = _endTime;
+      holdPeriod = _holdPeriod;
+    }
+
+    function mint(address _tokenHolder, uint256 _amount) public onlyOwner whileOpen {
+        TokenTimelock escrow = new TokenTimelock(ERC20Basic(this), _tokenHolder, now.add(holdPeriod));
+        holds[_tokenHolder] = escrow;
+        super.mint(escrow, _amount);
+    }
+
+    /// Releases hold on funds from TokenTimelock. Reverts if the hold time has not elapsed.
+    function releaseFor(address _tokenHolder) public { holds[_tokenHolder].release(); }
+    // Not removing from map saves gas.
+    // https://ethereum.stackexchange.com/questions/41576/is-it-cheaper-to-delete-or-ignore-obsolete-mappings
+
+
+    /// Reverts if not in crowdsale time range.
+    modifier whileOpen {
+      require(now >= startTime);
+      require(now <= endTime);
+      _;
+    }
 }
--- contracts/DealFactory.sol
+++ contracts/DealFactory.sol
@@ -3,13 +3,27 @@ pragma solidity ^0.4.15;
 import './Deal.sol';
 
 contract DealFactory {
-  event DealCreated(address sender, address instance);
+    event DealCreated(address sender, address instance);
 
-  mapping(address => address[]) public instantiations;
-  function createDeal(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) public returns (address deal) {
-    deal = new Deal(_startTime, _endTime, _rate, _wallet);
-    instantiations[msg.sender].push(deal);
-    DealCreated(msg.sender,deal);
-    return deal;
-  }
+    mapping(address => address[]) public registry;
+
+    function create(
+        string _name,
+        string _symbol,
+        uint256 _granularity,
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _holdPeriod,
+        TokenValidator _validator
+    ) public returns (address _deal) {
+        address deal =
+          new Deal(_name, _symbol, _granularity, _startTime, _endTime, _holdPeriod, _validator);
+
+        registry[msg.sender].push(deal);
+        DealCreated(msg.sender, deal);
+
+        return deal;
+    }
+
+    function mine() public view returns (address[]) { return registry[msg.sender]; }
 }
