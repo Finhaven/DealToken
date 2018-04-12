c9ac427699c54eae8956def5b02a1df79b610319
Brooklyn Zelenka <be.zelenka@gmail.com>
2018-04-12 00:19:30 -0700

    yey

--- contracts/MintHistory.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.21;
-
-library MintHistory {
-    struct Minting {
-      uint256 amount;
-      uint256 createdAt;
-    }
-}
---
+++ contracts/StaggeredDeal.sol
@@ -0,0 +1,88 @@
+pragma solidity ^0.4.19;
+/* pragma experimental ABIEncoderV2; */
+
+import "./Deal.sol";
+import "../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+/** Staggered close
+
+       startTime     endTime                     dealOver
+          -tX           t0                           tZ
+           |============|============================|
+Prehistory |  Mintable  |         Tradeable          | Inactive
+           |============|============================|
+           |            |                            |
+           |            |                            |
+     Alice |   a ---------- a + holdPeriod           |
+           |  Mint   Hold   Trade                    |
+           |            |                            |
+           |            |                            |
+       Bob |     b ---------- b + holdPeriod         |
+           |    Mint   Hold   Trade                  |
+           |            |                            |
+           |            |                            |
+     Carol |        c ---------- c + holdPeriod      |
+           |       Mint | Hold   Trade               |
+           |            |                            |
+
+ */
+contract StaggeredDeal is Deal, Ownable {
+    using SafeMath for uint256;
+
+    struct Minting {
+      uint256 amount;
+      uint256 createdAt;
+    }
+
+    // Indexed from 1, blugh
+    uint public holderCount;
+    mapping(uint => address) internal holderIndex;
+    mapping(address => uint) internal reverseHolderIndex;
+
+    mapping(address => Minting[]) internal mintHistory;
+
+    function StaggeredDeal(
+        string _name,
+        string _symbol,
+        uint256 _granularity,
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _holdPeriod,
+        TokenValidator _validator
+    ) Deal(_name, _symbol, _granularity, _startTime, _endTime, _holdPeriod, _validator) public {}
+
+    function mint(address _tokenHolder, uint256 _amount) external {
+        this.mint(_tokenHolder, _amount);
+
+        upsertHolder(_tokenHolder);
+
+        mintHistory[_tokenHolder].push(Minting({
+            amount: _amount,
+            createdAt: now
+        }));
+    }
+
+    function upsertHolder(address _tokenHolder) internal {
+        if (reverseHolderIndex[_tokenHolder] == 0) {
+            holderCount++;
+            holderIndex[holderCount] = _tokenHolder;
+            reverseHolderIndex[_tokenHolder] = holderCount;
+        }
+    }
+
+    function isEnoughSpendable(address _from, uint256 _amount) internal view returns (bool) {
+        return spendableAmount(_from, holdPeriod) >= _amount;
+    }
+
+    function spendableAmount(address _holder, uint256 _holdPeriod) internal view returns (uint256) {
+        uint256 total = 0;
+
+        for (uint i = 0; i < mintHistory[_holder].length; i++) {
+            if (now < mintHistory[_holder][i].createdAt.add(_holdPeriod)) {
+                total.add(mintHistory[_holder][i].amount);
+            }
+        }
+
+        return total;
+    }
+}
